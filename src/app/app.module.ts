125eb4b8c03e49a47af7ccb4ebab15c1380b73d2
Cinthia <votacinthia@gmail.com>
2023-02-24 16:36:57 -0300

    first commit - structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { LogoComponent } from './components/header/logo/logo.component';
+import { BannerComponent } from './components/header/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    NavbarComponent,
+    LogoComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
